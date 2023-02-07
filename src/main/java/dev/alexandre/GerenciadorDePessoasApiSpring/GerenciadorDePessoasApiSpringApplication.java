7e213f7126fe8f48ec34d623157ac527bf7daa07
Alexandre Rodrigues <alxrds@hotmail.com>
2023-02-06 21:51:13 -0300

    Iniciando projeto

---
+++ src/main/java/dev/alexandre/GerenciadorDePessoasApiSpring/GerenciadorDePessoasApiSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.alexandre.GerenciadorDePessoasApiSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDePessoasApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDePessoasApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alexandre/GerenciadorDePessoasApiSpring/GerenciadorDePessoasApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexandre.GerenciadorDePessoasApiSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDePessoasApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
